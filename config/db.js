c85258ae4bf5305e451175cac00832257ce5fa77
AsankaXtremeS <asankasampath200228@gmail.com>
2025-05-13 17:48:51 +0530

    Initial commit

---
+++ config/db.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/B23 E-Commerce Site`, opts).then((mongoose) => {
+            return mongoose
+        });
+    }
+    cached.conn = await cached.promise;
+    return cached.conn;
+}
+
+export default connectDB;
+// import mongoose from "mongoose";
