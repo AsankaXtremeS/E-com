db56d2e3b671e12f1cda28561110df57fe10f67a
AsankaXtremeS <asankasampath200228@gmail.com>
2025-05-19 23:34:40 +0530

    pasted new code to route

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,24 @@
-import { serve } from "inngest/next";
-import { inngest,syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
+// import { serve } from "inngest/next";
+// import { inngest,syncUserCreation, syncUserDeletion, syncUserUpdate } from "@/config/inngest";
 
-// Create an API that serves zero functions
+// // Create an API that serves zero functions
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   functions: [
+//     syncUserCreation,
+//     syncUserUpdate,
+//     syncUserDeletion
+//   ],
+// });
+import { serve } from "inngest/next";
+import {
+  inngest,
+  syncUserCreation,
+  syncUserUpdate,
+  syncUserDeletion
+} from "@/config/inngest"; // adjust path if needed
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    syncUserCreation,
-    syncUserUpdate,
-    syncUserDeletion
-  ],
+  functions: [syncUserCreation, syncUserUpdate, syncUserDeletion],
 });
