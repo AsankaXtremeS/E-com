3abce63c3a97d1f64d0159d42a01b398cd660026
AsankaXtremeS <asankasampath200228@gmail.com>
2025-06-01 20:44:42 +0530

    1.23

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,8 +1,10 @@
 "use client";
 import { productsDummyData, userDummyData } from "@/assets/assets";
-import { useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
+import axios from "axios";
 import { useRouter } from "next/navigation";
 import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
@@ -14,6 +16,8 @@ export const AppContextProvider = (props) => {
   const currency = process.env.NEXT_PUBLIC_CURRENCY;
   const router = useRouter();
   const { user } = useUser();
+
+  const {getToken} = useAuth();
 
   const [products, setProducts] = useState([]);
   const [userData, setUserData] = useState(false);
@@ -29,9 +33,22 @@ export const AppContextProvider = (props) => {
        if(user.publicMetadata.role === 'seller') {
       setIsSeller(true);
     }
-    setUserData(userDummyData);
+
+    const token = await getToken()
+
+   const { data } = await axios.get('/api/user/data', {
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+  });
+    if (data.success) {
+      setUserData(data.user)
+      setCartItems(data.user.cartItems)
+    }else{
+      toast.error(data.message)
+    }
    } catch (error) {
-    
+    toast.error(error.message)
    }
   };
 
@@ -87,7 +104,7 @@ export const AppContextProvider = (props) => {
   }, [user]);
 
   const value = {
-    user,
+    user,getToken,
     currency,
     router,
     isSeller,
