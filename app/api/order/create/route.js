3e7c46996b3d871f8999731880b20d6243965f7d
AsankaXtremeS <asankasampath200228@gmail.com>
2025-06-04 20:06:18 +0530

    g

--- app/api/order/create/route.js
+++ app/api/order/create/route.js
@@ -57,7 +57,7 @@ export async function POST(request) {
                 success: false,
                 message: error.message,
             },
-            { status: 504 }
+            { status: 500 }
         );
     }
 }
